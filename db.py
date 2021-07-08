c294a5220714a72987bf7e2446595f5937fa283d
Eryk <36778031+eryktr@users.noreply.github.com>
2021-07-08 19:50:01 +0200

    Implemented dummy db service (#2)
     Implemented dummy db service

---
+++ db.py
@@ -0,0 +1,17 @@
+import abc
+import models
+
+
+class DbService(abc.ABC):
+    @abc.abstractmethod
+    def get_users(self) -> list[models.User]:
+        ...
+
+
+class DummyDbService(DbService):
+    _USERS = [
+        models.User(uid=1, username="Noob", password="12345"),
+    ]
+
+    def get_users(self) -> list[models.User]:
+        return self._USERS
