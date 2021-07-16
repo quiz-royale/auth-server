21bc69d4f9c2d07be190b3e6bb342d3ed190357f
KirsteinDamian <kirsteindamian@gmail.com>
2021-07-16 18:23:20 +0200

    Update quizroyale/db/errors.py
    Co-authored-by: Eryk <36778031+eryktr@users.noreply.github.com>

--- quizroyale/db/errors.py
+++ quizroyale/db/errors.py
@@ -1,4 +1,4 @@
 class NoSuchUserException(Exception):
-    def __init__(self):
+    def __init__(self) -> None:
         self.message = 'Invalid username or password!'
         super().__init__(self.message)
